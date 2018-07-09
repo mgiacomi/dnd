1fe5805435f087aa859efcc172a369d20eafdfba
Cain Giacomini <cain@gltech.com>
2018-07-09 16:45:10 -0700

    Making sure commit works.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-character = Character.new(name: 'Matt', classy: 'Human',)
+
+
